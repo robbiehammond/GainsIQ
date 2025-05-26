2e77dcaf00cc10defeb485f44ebbec6fcca42ae3
Robbie Hammond <robbiehammond3@gmail.com>
2025-05-25 22:16:17 -0700

    fix build script to build bootstrap

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -2,11 +2,8 @@ cd frontend
 npm install
 npm run build:preprod
 cd ..
-#cd backend
-#cargo lambda build --release --arm64 
-#cd .. 
 echo "Building Go Lambda for preprod environment"
 cd backend/go
-GOOS=linux GOARCH=amd64 go build -ldflags=\"-s -w\" -o main main.go
+GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o bootstrap main.go
 cd ../..
 cdk deploy -c env=preprod
