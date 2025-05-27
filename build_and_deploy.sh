39c489f01a1a4ca74b478963de2a61fc7e403011
Robbie Hammond <robbiehammond3@gmail.com>
2025-05-27 14:47:25 -0700

    add backend tests

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -5,5 +5,6 @@ cd ..
 echo "Building Go Lambda for preprod environment"
 cd backend/go
 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o bootstrap main.go
+EXERCISES_TABLE=test SETS_TABLE=test WEIGHT_TABLE=test ANALYSES_TABLE=test QUEUE_URL=test API_KEY_MAP='{}' go test -v
 cd ../..
 cdk deploy -c env=preprod
--- build_and_deploy_prod.sh
+++ build_and_deploy_prod.sh
@@ -6,5 +6,6 @@ cd ..
 echo "Building Go Lambda for prod environment"
 cd backend/go
 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o bootstrap main.go
+EXERCISES_TABLE=test SETS_TABLE=test WEIGHT_TABLE=test ANALYSES_TABLE=test QUEUE_URL=test API_KEY_MAP='{}' go test -v
 cd ../..
 cdk deploy
